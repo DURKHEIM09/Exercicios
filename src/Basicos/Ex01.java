3b4b8b574a99237f7e6f1e66fb975203f3679ecf
DURKHEIM09 <106775719+DURKHEIM09@users.noreply.github.com>
2022-12-15 09:34:03 -0300

    Add files via upload

---
+++ src/Basicos/Ex01.java
@@ -0,0 +1,32 @@
+package Operadores.Desafio;
+
+public class Ex01 {
+	public static void main(String[] args) {
+	
+		 
+		int A = 6 * (3 + 2);
+	    int AA = (int) Math.pow(A, 2);
+	    
+	    int B = 3 * 2;
+	    int BB = AA / B;
+	    
+	    int C = (1-5) * (2-7) / 2;
+	    int CC = (int) Math.pow(C, 2);
+		
+	    int D = BB - CC;
+	    int DD = (int) Math.pow(D, 3);
+		
+		int E = 10;
+		int EE = (int) Math.pow(E, 3);
+		
+		int F = DD / EE;
+		System.out.println(F);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
